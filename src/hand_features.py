1771293f689b4df89113ef06446963602b1f7e7c
bnies1017 <ben.nies1017@gmail.com>
2025-10-05 15:13:36 -0600

    refactored helpers into equity and hand features

--- src/hand_features.py
+++ src/hand_features.py
@@ -1,34 +1,7 @@
-from math import comb
 from deuces import Card, Evaluator
 import pandas as pd
 
-# total_hands is the number of ways to choose (players-1) opponents from 8 possible opponents
-def total_hands(players):
-    """
-    Calculate the total number of possible hands given the number of players.
-    8 choose (players-1)
-    8 possible opponents (excluding the player)
-    :param players: Integer representing the number of players (including the player).
-    2 <= players <= 9
-    :return: Integer representing the total number of possible hands.
-    """
-    return comb(8, players - 1)
 
-# wins is the number of ways to choose (players-1) opponents from (9-showdown_order) possible opponents
-def wins(showdown_order, players):
-    """
-    Calculate the number of winning hands given the showdown order and number of players.
-    (9 - showdown_order) choose (players - 1)
-    9 - showdown_order possible opponents (excluding the player and those who have already shown down
-    :param showdown_order: Integer representing the order in which the player's hand was ranked among 9 other hands
-     (1-based).
-    1 <= showdown_order <= 9
-    :param players: Integer representing the number of players (including the player).
-    2 <= players <= 9
-    :return: Integer representing the number of winning hands.
-    0 <= wins <= total_hands
-    """
-    return comb(9 - showdown_order, players - 1)
 
 evaluator = Evaluator()
 
